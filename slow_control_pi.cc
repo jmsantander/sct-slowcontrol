cfd8f5f39276f1e4e951453b9ffa0bdc533e1c2d
Ari Brill <aryeh.brill@gmail.com>
2016-06-20 14:49:57 -0400

    Use high-level networking setup and shutdown in pi and server programs

--- slow_control_pi.cc
+++ slow_control_pi.cc
@@ -4,66 +4,73 @@
 
 #include <iostream>
 #include <string>
-#include <unistd.h>
-#include <cstdlib>
+//#include <unistd.h>
+//#include <cstdlib>
 
 #include "sc_network.h"
 #include "sc_protobuf.pb.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
     // Verify that the version of the Protocol Buffer library we linked against
     // is compatible with the version of the headers we compiled against.
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
+    // Parse command line arguments
+    if (argc != 2) {
+        fprintf(stderr, "usage: slow_control_pi hostname\n");
+        return 1;
+    }
+    std::string hostname = argv[1];
+    
     // Set up protocol buffer
     slow_control::Backplane backplane;
 
+    // Set up networking info
+    Network_info netinfo(hostname);
+
     // Connect to the server
-    std::cout << "connecting to the server...\n";
-    int server = -1;
-    if (!open_connection(server))
+    if (!setup_network(netinfo, PI))
         return 1;
 
-    // Communicate with the server: on each loop send updated data and 
-    // recieve updated settings
-    std::cout << "communicating with the server...\n";
-    std::string message;
-    srand(54321); // Initialize random seed
-    for (int i = 0; i < 10; i++) {
-        std::cout << "Iteration " << i << std::endl;
-        // Set data to random numbers
-        backplane.set_voltage(rand() % 10);
-        backplane.set_current(rand() % 10);
-        if (!backplane.SerializeToString(&message))
-            return 1;
-        if (!send_message(server, message))
-            return 1;
-        sleep(1);
-        // Set settings to those sent by server
-        if (!receive_message(server, message)) {
-            // End communication to the server when no more messages
-            std::cout << "breaking the connection.\n";
-            close_connection(server);
-            return 1;
-        }
-        if (!backplane.ParseFromString(message))
-            return 1;
-        // Display updated values
-        std::cout << "updated voltage is: " << backplane.voltage() 
-            << std::endl;
-        std::cout << "updated current is: " << backplane.current() 
-            << std::endl;
-        std::cout << "updated desired voltage is: "
-            << backplane.desired_voltage() << std::endl;
-        std::cout << "updated desired current is: "
-            << backplane.desired_current() << std::endl;
-    }
+    // Shut down network
+    if (!shutdown_network(netinfo))
+        return 1;
 
-
-    // End communication to the server
-    std::cout << "breaking the connection.\n";
-    close_connection(server);
+//    // Communicate with the server: on each loop send updated data and 
+//    // recieve updated settings
+//    std::cout << "communicating with the server...\n";
+//    std::string message;
+//    srand(54321); // Initialize random seed
+//    for (int i = 0; i < 10; i++) {
+//        std::cout << "Iteration " << i << std::endl;
+//        // Set data to random numbers
+//        backplane.set_voltage(rand() % 10);
+//        backplane.set_current(rand() % 10);
+//        if (!backplane.SerializeToString(&message))
+//            return 1;
+//        if (!send_message(server, message))
+//            return 1;
+//        sleep(1);
+//        // Set settings to those sent by server
+//        if (!receive_message(server, message)) {
+//            // End communication to the server when no more messages
+//            std::cout << "breaking the connection.\n";
+//            close_connection(server);
+//            return 1;
+//        }
+//        if (!backplane.ParseFromString(message))
+//            return 1;
+//        // Display updated values
+//        std::cout << "updated voltage is: " << backplane.voltage() 
+//            << std::endl;
+//        std::cout << "updated current is: " << backplane.current() 
+//            << std::endl;
+//        std::cout << "updated desired voltage is: "
+//            << backplane.desired_voltage() << std::endl;
+//        std::cout << "updated desired current is: "
+//            << backplane.desired_current() << std::endl;
+//    }
 
     return 0;
 }
--- slow_control_server.cc
+++ slow_control_server.cc
@@ -7,65 +7,59 @@
 #include "sc_network.h"
 #include "sc_protobuf.pb.h"
 
-int main(int argc, char *argv[])
+int main(void)
 {
     // Verify that the version of the library we linked against is compatible
     // with the version of the headers we compiled against
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    // Parse command line arguments
-    if (argc != 2) {
-        fprintf(stderr, "usage: slow_control_server hostname\n");
-        return 1;
-    }
-    char *hostname = argv[1];
-
     // Set up protocol buffer
     slow_control::Backplane backplane;
     
-    // Connect to the Pi
-    std::cout << "connecting to the pi...\n";
-    int pi = -1; 
-    if (!open_connection(pi, hostname))
+    // Set up networking info
+    Network_info netinfo;
+    
+    // Connect to the Pi and GUI
+    if (!(setup_network(netinfo, SERVER)))
+        return 1;
+    
+    // Shut down network
+    if (!shutdown_network(netinfo))
         return 1;
 
-    // Communicate with the Pi: on each loop receive updated data and 
-    // send updated settings
-    std::cout << "communicating with the Pi...\n";
-    std::string message;
-    srand(12345); // Initialize random seed
-    for (int i = 0; i < 10; i++) {
-        std::cout << "Iteration " << i << std::endl;
-        // Set data to those sent by Pi
-        if (!receive_message(pi, message)) {
-            // End communication to the Pi when no more messages
-            std::cout << "breaking the connection.\n";
-            close_connection(pi);
-            return 1;
-        }
-        if (!backplane.ParseFromString(message))
-            return 1;
-        // Set settings to random numbers
-        backplane.set_desired_voltage(rand() % 10);
-        backplane.set_desired_current(rand() % 10);
-        if (!backplane.SerializeToString(&message))
-            return 1;
-        if (!send_message(pi, message))
-            return 1;
-        // Display updated values
-        std::cout << "updated voltage is: " << backplane.voltage() 
-            << std::endl;
-        std::cout << "updated current is: " << backplane.current() 
-            << std::endl;
-        std::cout << "updated desired voltage is: "
-            << backplane.desired_voltage() << std::endl;
-        std::cout << "updated desired current is: "
-            << backplane.desired_current() << std::endl;
-    }
-    
-    // End communication to the Pi
-    std::cout << "breaking the connection.\n";
-    close_connection(pi);
+//    // Communicate with the Pi: on each loop receive updated data and 
+//    // send updated settings
+//    std::cout << "communicating with the Pi...\n";
+//    std::string message;
+//    srand(12345); // Initialize random seed
+//    for (int i = 0; i < 10; i++) {
+//        std::cout << "Iteration " << i << std::endl;
+//        // Set data to those sent by Pi
+//        if (!receive_message(pi, message)) {
+//            // End communication to the Pi when no more messages
+//            std::cout << "breaking the connection.\n";
+//            close_connection(pi);
+//            return 1;
+//        }
+//        if (!backplane.ParseFromString(message))
+//            return 1;
+//        // Set settings to random numbers
+//        backplane.set_desired_voltage(rand() % 10);
+//        backplane.set_desired_current(rand() % 10);
+//        if (!backplane.SerializeToString(&message))
+//            return 1;
+//        if (!send_message(pi, message))
+//            return 1;
+//        // Display updated values
+//        std::cout << "updated voltage is: " << backplane.voltage() 
+//            << std::endl;
+//        std::cout << "updated current is: " << backplane.current() 
+//            << std::endl;
+//        std::cout << "updated desired voltage is: "
+//            << backplane.desired_voltage() << std::endl;
+//        std::cout << "updated desired current is: "
+//            << backplane.desired_current() << std::endl;
+//    }
     
     return 0;
 }
